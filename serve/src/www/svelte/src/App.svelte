4850dca8b0a42a9ff393e6d14ad37fe5944dd577
Mike Moran <mike@houseofmoran.com>
2019-07-28 14:54:31 +0200

    extract GridStretch component

--- serve/src/www/svelte/src/App.svelte
+++ serve/src/www/svelte/src/App.svelte
@@ -1,25 +1,9 @@
 <script>
-  export let strength;
-
-  $: console.log("strength", strength);
-
-  function stretchToGrid() {
-    strength = 1.0;
-  }
+  import GridStretch from "./GridStretch.svelte";
 </script>
 
 <style>
 
 </style>
 
-<input
-  id="strength"
-  type="range"
-  min="0.0"
-  max="1.0"
-  step="0.01"
-  bind:value={strength}
-  style="width: 50%" />
-<label for="strength">{strength}</label>
-
-<button on:click={stretchToGrid}>Stretch to real grid</button>
+<GridStretch />
---
+++ serve/src/www/svelte/src/GridStretch.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let stretch = 0.0;
+
+  $: console.log("stretch", stretch);
+
+  function stretchToGrid() {
+    stretch = 1.0;
+  }
+</script>
+
+<style>
+
+</style>
+
+<input
+  id="stretch"
+  type="range"
+  min="0.0"
+  max="1.0"
+  step="0.01"
+  bind:value={stretch}
+  style="width: 50%" />
+<label for="strength">{stretch}</label>
+<button on:click={stretchToGrid}>Stretch to real grid</button>
