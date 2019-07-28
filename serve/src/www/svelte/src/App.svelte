fb551010f79de34fb077f746276999696847269a
Mike Moran <mike@houseofmoran.com>
2019-07-28 15:05:06 +0200

    load data using d3-fetch

--- serve/src/www/svelte/src/App.svelte
+++ serve/src/www/svelte/src/App.svelte
@@ -1,5 +1,11 @@
 <script>
   import GridStretch from "./GridStretch.svelte";
+
+  import { json } from "d3-fetch";
+
+  json("/layout.json").then(function(data) {
+    console.log(data);
+  });
 </script>
 
 <style>
