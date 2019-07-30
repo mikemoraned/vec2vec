4887c3723d6cc9d6cf3f7500c2981abc6841cf4e
Mike Moran <mike@houseofmoran.com>
2019-07-30 16:01:19 +0200

    some state for stretch

--- serve/src/www/react-app/src/App.js
+++ serve/src/www/react-app/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function StretchComponent({ stretch, setStretch, setMaximumStretch }) {
+  return (
+    <div className="StretchComponent">
+      <span>{stretch}</span>
+      <button onClick={setMaximumStretch}>Stretch to Grid</button>
+    </div>
+  );
+}
 
 function App() {
+  const [stretch, setStretch] = useState(0.0);
+
+  function setMaximumStretch() {
+    setStretch(1.0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StretchComponent
+        stretch={stretch}
+        setMaximumStretch={setMaximumStretch}
+      />
     </div>
   );
 }
